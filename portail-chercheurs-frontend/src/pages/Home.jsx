c2755d8ff85a7b3a36501fd30e858cdea6c46076
Ouassim721 <derjaouassim2004@gmail.com>
2025-04-03 11:43:55 +0000

    home page placeholder

--- portail-chercheurs-frontend/src/pages/Home.jsx
+++ portail-chercheurs-frontend/src/pages/Home.jsx
@@ -1,67 +1,17 @@
 function Home() {
   return (
     <>
-      <h1>Bienvenue sur le portail chercheurs</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, possimus
-        animi et ipsam est, fugiat tempore debitis beatae ab reiciendis saepe
-        consectetur cupiditate nihil eligendi reprehenderit ducimus soluta
-        doloribus perspiciatis!
-      </p>
+      {/* Contenu principal */}
+      <main className="flex flex-col items-center justify-center flex-grow px-6 h-[500px]">
+        <h2 className="text-3xl font-bold mb-4 text-center">
+          Bienvenue sur le Portail des Chercheurs [Page en cours de
+          développement]
+        </h2>
+        <p className="text-gray-700 text-lg text-center max-w-2xl">
+          Accédez aux profils des chercheurs, découvrez leurs travaux et
+          explorez les collaborations possibles.
+        </p>
+      </main>
     </>
   ); // Contenu de la page Home
 }
